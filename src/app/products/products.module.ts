a6e630aa4a7825f9f95821565848ddc6c4ecf3f2
Cocodup <dreexys@hotmail.fr>
2023-07-12 17:13:39 +0200

    Creations pages

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ProductsRoutingModule } from './products-routing.module';
+import { PageListProductsComponent } from './pages/page-list-products/page-list-products.component';
+import { PageAddProductComponent } from './pages/page-add-product/page-add-product.component';
+import { PageEditProductComponent } from './pages/page-edit-product/page-edit-product.component';
+import { ProductFormComponent } from './product-form/product-form.component';
+
+
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PageListProductsComponent,
+    PageAddProductComponent,
+    PageEditProductComponent,
+    ProductFormComponent,
+    
+  ],
   imports: [
     CommonModule,
     ProductsRoutingModule
