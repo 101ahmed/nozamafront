86bd83a723f217e89e30a3a59188cee799e7f180
Sarah_Serrandour <Sarahserrandour@yahoo.com>
2023-07-13 12:17:04 +0200

    création page edit

--- src/app/products/pages/page-edit-product/page-edit-product.component.ts
+++ src/app/products/pages/page-edit-product/page-edit-product.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ProductsService } from '../../services/products.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-page-edit-product',
   templateUrl: './page-edit-product.component.html',
   styleUrls: ['./page-edit-product.component.scss']
 })
-export class PageEditProductComponent {
+export class PageEditProductComponent implements OnInit {
 
+  @Input() id!: number;
+
+  product$!: Observable<Products>;
+
+  constructor(
+    private productsService: ProductsService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.product$ = this.productsService.getProductbyId(this.id);
+  }
+
+  // protected readonly faSpinner = this.faSpinner;
+
+  getToProductsList() : void {
+    this.router.navigate(['/', 'products','list']);
+  }
+
+  updateProduct(product : Products) : void {
+    
+    this.productsService.putProduct({...product, id: this.id}).subscribe(
+      (res: Products) : void=> {
+        this.getToProductsList();
+      }
+    );
+  }
 }
