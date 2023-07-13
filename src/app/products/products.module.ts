f9d2155c409bcd45c8403e94a8819aeaf86112a9
Lou Cadau <loucadau22@orange.fr>
2023-07-13 12:25:07 +0000

    Update file products.module.ts

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SharedModule } from '../shared/shared.module';
 
 import { ProductsRoutingModule } from './products-routing.module';
 import { PageListProductsComponent } from './pages/page-list-products/page-list-products.component';
