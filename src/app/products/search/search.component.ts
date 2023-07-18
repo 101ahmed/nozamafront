4eb27cfefee4407e1c5ef114aff482c294249b79
Lou22110 <loucadau22@orange.fr>
2023-07-18 10:34:43 +0200

    V2 Catégories

--- src/app/products/search/search.component.ts
+++ src/app/products/search/search.component.ts
@@ -8,7 +8,6 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 export class SearchComponent implements OnInit{
   constructor(){}
   ngOnInit(): void {
-    
   }
 
   enteredSearchValue: string ='';
