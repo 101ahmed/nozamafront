52c89aff3f694a95f4009fe9caffe46c73b54961
TDuchesne <duchesne.thomas@hotmail.fr>
2023-07-18 14:59:35 +0200

    Correction user + db.json à jour + routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {path: 'products', loadChildren: () => import('./products/products.module').then(m => m.ProductsModule)},
   {path: 'orders', loadChildren:() => import('./orders/orders.module').then(m => m.OrdersModule )},
-  {path: 'categories', loadChildren:() => import('./categories/categories.module').then(m => m.CategoriesModule )}
+  {path: 'categories', loadChildren:() => import('./categories/categories.module').then(m => m.CategoriesModule )},
+  {path: 'users', loadChildren:() => import('./users/users.module').then(m => m.UsersModule )}
 
 ];
 
