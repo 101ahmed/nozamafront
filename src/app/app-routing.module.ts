eab98b0350b18160da90388d0c37c79a02deb8f2
Cocodup <dreexys@hotmail.fr>
2023-07-19 16:53:30 +0200

    catalogue

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,8 @@ const routes: Routes = [
   {path: 'products', loadChildren: () => import('./products/products.module').then(m => m.ProductsModule)},
   {path: 'orders', loadChildren:() => import('./orders/orders.module').then(m => m.OrdersModule )},
   {path: 'categories', loadChildren:() => import('./categories/categories.module').then(m => m.CategoriesModule )},
-  {path: 'users', loadChildren:() => import('./users/users.module').then(m => m.UsersModule )}
+  {path: 'users', loadChildren:() => import('./users/users.module').then(m => m.UsersModule )},
+  {path: 'catalogue', loadChildren:() => import('./catalogue/catalogue.module').then(m => m.CatalogueModule)},
 
 ];
 
