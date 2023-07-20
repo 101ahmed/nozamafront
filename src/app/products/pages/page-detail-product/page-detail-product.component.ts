13e7ca72a2f1a6e5a9b9a12a1f835737b2276c9a
unknown <alnonoahmed16@gmail.com>
2023-07-20 16:39:30 +0200

    dietai product V3

--- src/app/products/pages/page-detail-product/page-detail-product.component.ts
+++ src/app/products/pages/page-detail-product/page-detail-product.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Product } from '../../models/product';
+import { ProductsService } from '../../services/products.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-page-detail-product',
   templateUrl: './page-detail-product.component.html',
   styleUrls: ['./page-detail-product.component.scss']
 })
-export class PageDetailProductComponent {
+export class PageDetailProductComponent implements OnInit{
+  @Input() id!: number;
+  product$!: Observable<Product>;
+  constructor(
+    private productsService: ProductsService,
+    private router: Router
+  ) {}
 
+  ngOnInit(): void {
+      
+    this.product$ = this.productsService.getProductById(this.id);
+  }
 }
