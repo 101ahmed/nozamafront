c74a3d4c2d3d133c2afbc0f1b89eddd977d947c3
unknown <alnonoahmed16@gmail.com>
2023-07-20 02:27:54 +0200

    pages products is ready

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { PageDetailProductComponent } from './products/pages/page-detail-product/page-detail-product.component';
 
 const routes: Routes = [
   {path: 'products', loadChildren: () => import('./products/products.module').then(m => m.ProductsModule)},
+ // { path: 'detail/:id', component: PageDetailProductComponent},
   {path: 'orders', loadChildren:() => import('./orders/orders.module').then(m => m.OrdersModule )},
   {path: 'categories', loadChildren:() => import('./categories/categories.module').then(m => m.CategoriesModule )},
   {path: 'users', loadChildren:() => import('./users/users.module').then(m => m.UsersModule )},
